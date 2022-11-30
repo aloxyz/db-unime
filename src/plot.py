82eb2a0a3989b2ce9a8a6808b03d07f5348af502
aloxyz <aloisiogabriele@gmail.com>
2022-11-30 11:47:30 +0100

    plot

--- src/plot.py
+++ src/plot.py
@@ -1,7 +1,7 @@
-import os
 import matplotlib.pyplot as plt
 import numpy as np
 import csv
+import math
 
 mongo = []
 neo = []
@@ -19,13 +19,13 @@ for i in range(25, 101, 25):
         plots = csv.reader(csvfile, delimiter = ',')
         
         mean = list(plots)[31]
-        mongo.append(mean[1])
+        mongo.append(float(mean[1]) * 1000)
 
     with open(f_neo,'r') as csvfile:
         plots = csv.reader(csvfile, delimiter = ',')
         
         mean = list(plots)[31]
-        neo.append(mean[1])
+        neo.append(float(mean[1]) * 1000)
 
 
 
@@ -38,15 +38,18 @@ fig, ax = plt.subplots()
 
 rects1 = ax.bar(x - width/2, neo, width, label='Neo4j')
 rects2 = ax.bar(x + width/2, mongo, width, label='MongoDB')
-ax.bar_label(rects1, padding=3)
-ax.bar_label(rects2, padding=3)
+
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
+
 ax.set_ylabel('Tempo (Millisecondi)')
 ax.set_title('2 WHERE')
 ax.set_xticks(x, labels)
 ax.legend()
 
+ax.bar_label(rects1, padding=3)
+ax.bar_label(rects2, padding=3)
+
 fig.tight_layout()
-plt.legend()
+
 plt.show()
